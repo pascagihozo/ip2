e1cf043f19f25028bbe8779a1b4a981d185c49de
BRIDGETCHIRIMBANI <b.chirimban@alustudent.com>
2023-08-04 18:31:19 +0400

    runnable

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,17 +5,18 @@ import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom/dist";
 
 const Header = () => {
+
   return (
-    <Navbar expand="lg" bg="primary" variant="dark" fixed="top">
-      <Container fluid className="p-4">
-        <Navbar.Brand href="#home">Power6</Navbar.Brand>
+    <Navbar expand="lg" bg="dark" className="text-light" fixed="top">
+      <Container fluid className="p-2">
+        <Navbar.Brand className="text-light" href="/">Power6</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="authors">Authors</NavLink>
-            <NavLink to="sources">Data source</NavLink>
-            <NavLink to="visualspage">Visualization</NavLink>
+            <NavLink className='ml-2' to="/">Home</NavLink>
+            <NavLink className='ml-2' to="authors">Authors</NavLink>
+            <NavLink className='ml-2' to="sources">Data source</NavLink>
+            <NavLink className='ml-2' to="visualspage">Visualization</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
